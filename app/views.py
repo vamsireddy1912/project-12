25774af2bb0c96fb0ffe72595b033f89f4e7ba7f
vamsireddy1912 <kapuramvamsi1912@gmail.com>
2023-11-22 15:42:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vamsi(request):
+    return render(request,'vamsi.html')
+
+
+def charan(request):
+    return render(request,'charan.html')
